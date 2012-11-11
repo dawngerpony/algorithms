70e7fcf0be0b60855fdaa0f04378afb04d7c483a
Dafydd James <dafydd@cantab.net>
2012-11-11 21:16:56 +0000

    Linked list queue implementation

---
+++ src/com/duffj/algorithms/misc/Queue.java
@@ -0,0 +1,68 @@
+package com.duffj.algorithms.misc;
+
+/**
+ * FIFO queue implementation using a single linked list.
+ * 
+ * @author dafydd
+ *
+ * @param <Item>
+ */
+public class Queue<Item> {
+	private Node first;
+	private Node last;
+	private int N;
+	
+	/**
+	 * Singly linked list.
+	 * 
+	 * @author dafydd
+	 *
+	 */
+	private class Node {
+		Item item;
+		Node next;
+	}
+	
+	public boolean isEmpty() {
+		return first == null;
+	}
+	
+	public int size() {
+		return N;
+	}
+	
+	public void enqueue(Item item) {
+		Node oldlast = last;
+		last = new Node();
+		last.item = item;
+		last.next = null;
+		if(isEmpty()) {
+			first = last;
+		} else {
+			oldlast.next = last;			
+		}
+		N++;
+	}
+	
+	public Item dequeue() {
+		Item item = first.item;
+		first = first.next;
+		N--;
+		if(isEmpty()) {
+			last = null;
+		}
+		return item;
+	}
+	
+	public static void main(String[] args) {
+		Queue<String> q = new Queue<String>();
+		String test = "BEAUTIFUL";
+		for(int i = 0; i < test.length(); i++) {
+			String item = "" + test.charAt(i);
+			q.enqueue(item);
+		}
+		while(!q.isEmpty()) {
+			System.out.println(q.dequeue());
+		}
+	}
+}
