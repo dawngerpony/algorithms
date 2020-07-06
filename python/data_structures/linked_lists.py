09f1faa6eeb95cba6ae758462102298199e5b4a5
Dawn James <dawnjames2105@gmail.com>
2020-07-06 20:46:52 +0100

    Fix insert method and add more tests.

--- python/data_structures/linked_lists.py
+++ python/data_structures/linked_lists.py
@@ -9,7 +9,10 @@ class SinglyLinkedList:
 
     def insert(self, value):
         new_node = self.Node(value)
-        self._head.next(new_node)
+        curr = self._head
+        while curr.next():
+            curr = curr.next()
+        curr.next(new_node)
 
     def search(self, value):
         curr = self._head
@@ -50,26 +53,35 @@ class SinglyLinkedList:
             return str(self._value)
 
 
-class DoubleNode:
-    """ A node in a doubly linked list. """
+class DoublyLinkedList:
+    """ A doubly linked list. """
 
-    def __init__(self, item, next_node=None, prev_node=None):
-        self._value = item
-        self._next = next_node
-        self._prev = prev_node
+    def __init__(self, value=None):
+        self._head = self.Node(value)
 
-    def link(self, next_node):
-        """ Link the next node to the current node. """
-        self._next = next_node
-        next_node.link(self)
+    def insert(self, value):
+        pass
 
-    def next(self):
-        """ Get the next node. """
-        return self._next
+    class Node:
+        """ A node in a doubly linked list. """
 
-    def prev(self):
-        """ Get the previous node. """
-        return self._prev
+        def __init__(self, item, next_node=None, prev_node=None):
+            self._value = item
+            self._next = next_node
+            self._prev = prev_node
 
-    def __str__(self):
-        return str(self._value)
+        def link(self, next_node):
+            """ Link the next node to the current node. """
+            self._next = next_node
+            next_node.link(self)
+
+        def next(self):
+            """ Get the next node. """
+            return self._next
+
+        def prev(self):
+            """ Get the previous node. """
+            return self._prev
+
+        def __str__(self):
+            return str(self._value)
--- python/tests/test_linked_lists.py
+++ python/tests/test_linked_lists.py
@@ -9,17 +9,28 @@ class TestSinglyLinkedList:
         lst = SinglyLinkedList()
         assert len(lst) == 1
 
-    def test_insert(self):
+    def test_insert_one(self):
         """ Test the insert method. """
         lst = SinglyLinkedList('apple')
         lst.insert('banana')
         assert len(lst) == 2
 
+    def test_insert_two(self):
+        """ Test the insert method. """
+        lst = SinglyLinkedList('apple')
+        assert len(lst) == 1
+        lst.insert('banana')
+        assert len(lst) == 2
+        lst.insert('cherry')
+        assert len(lst) == 3
+
     def test_search_success(self):
         """ Test a successful set of searches. """
         lst = SinglyLinkedList('apple')
         lst.insert('banana')
-        node = lst.search('apple')
-        assert node.value() == 'apple'
-        node = lst.search('banana')
-        assert node.value() == 'banana'
+        lst.insert('cherry')
+        lst.insert('damson')
+        for fruit in ['apple', 'banana', 'cherry', 'damson']:
+            node = lst.search(fruit)
+            assert node.value() == fruit
+        assert lst.search('pineapple') is None
