1cc12ab0fb60763325aaacb6caae5ba140621eb5
Dawn James <dawnjames2105@gmail.com>
2020-07-06 19:33:34 +0100

    Add linting.

--- python/tests/test_bag.py
+++ python/tests/test_bag.py
@@ -10,3 +10,15 @@ def test_bag_creation_is_not_empty():
     b = Bag()
     b.add(0)
     assert not b.is_empty()
+
+
+def test_bag_len():
+    b = Bag()
+    assert len(b) == 0
+    b.add(0)
+    assert len(b) == 1
+
+
+def test_bag_repr():
+    b = Bag()
+    assert repr(b) == "Bag([])"
