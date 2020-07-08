81b7ae939245a58a7c6d94dec7d7ccf243ab2fdc
Dawn James <dawnjames2105@gmail.com>
2020-07-08 08:44:21 +0100

    Rewrite test to be more elegant using ArrayUtils.

--- java/src/test/java/com/dawngerpony/algorithms/sort/SelectionSortTest.java
+++ java/src/test/java/com/dawngerpony/algorithms/sort/SelectionSortTest.java
@@ -1,6 +1,11 @@
 package com.dawngerpony.algorithms.sort;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -8,5 +13,18 @@ public class SelectionSortTest {
     @Test
     public void testAdd() {
         assertEquals(42, Integer.sum(19, 23));
+    }
+
+    @Test
+    public void testSortSuccess() {
+        String unsorted = "cba";
+        String expected = "abc";
+        Character[] c = ArrayUtils.toObject(unsorted.toCharArray());
+
+        SelectionSort sorter = new SelectionSort();
+        Character[] sorted = (Character[]) sorter.sort(c);
+
+        String actual = new String(ArrayUtils.toPrimitive(sorted));
+        assertEquals(expected, actual);
     }
 }
